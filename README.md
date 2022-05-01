cb355879bb2f5f11a487577a9eccd02700b644f6
Karsten Løgstrup <karstenlogstrup@gmail.com>
2022-05-01 21:02:15 +0200

    A new weater app coming up

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Vejret
+# Min egen app der viser vejret
+
---
+++ app.js
@@ -0,0 +1,36 @@
+
+const express = require("express");
+const https = require ("https");
+const app = express();
+
+
+/*GET funktion. */
+app.get("/", function(req,res){
+
+    const query =
+      "https://api.openweathermap.org/data/2.5/weather?appid=99fcd1599ec1c9c403961abde484fd86&lang=da&q=copenhagen&units=metric";
+
+https.get(query, function (response) {
+      console.log(response.statusCode);
+
+      response.on("data", function (data) {
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const feels = weatherData.main.feels_like;
+        const weatherDescription = weatherData.weather[0].description;
+
+        console.log(temp);
+      });
+    });
+
+/*Herunder er hvad der vises i browseren*/
+    
+    res.send("App is up and running")
+});
+
+
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000")
+});
+
